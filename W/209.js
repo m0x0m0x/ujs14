1aefc5adcf30e30ff113628b080382caf75b46fe
m0x0m0x <mx0mx@tuta.io>
2024-04-25 20:55:29 +0530

    add new funtion

--- W/209.js
+++ W/209.js
@@ -1,8 +1,31 @@
+"use strict";
+
 /* 
 209: Constructor Functions and the new Operator
 */
 // Import zone
-import { mainHead } from "../UTILS/text.js";
+import { mainHead, subHead } from "../UTILS/text.js";
 // End import zone
 mainHead("209: Constructor Functions and the new Operator");
 console.log("hello");
+
+////////////////////////////////////
+
+subHead("Create new constructor function for a person");
+
+const Person = function (firstName, birthYear, fetish) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  this.fetish = fetish;
+};
+
+const ina = new Person("Ina", 2000, "fart");
+console.log(ina);
+
+/* 
+4 step happen when using the new keyword
+1. New {} empty object created 
+2. Function is called , 'this' keyword set to new created object
+3. {} empty object is linked to a prototype 
+4. function automatically returns empty object {}
+*/
