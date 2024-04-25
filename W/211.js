44d48315819f097aba07c1fba1df3b82375261b9
m0x0m0x <mx0mx@tuta.io>
2024-04-25 22:48:34 +0530

    211

---
+++ W/211.js
@@ -0,0 +1,10 @@
+"use strict";
+
+/* 
+211: Prototype inhertitance and Prototype Chain
+*/
+// Import zone
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+// End import zone
+mainHead("211: Prototype inhertitance and Prototype Chain");
+console.log("hello");
