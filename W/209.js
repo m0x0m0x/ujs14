e94dc17f12328177859af179c271a446f39a9a32
m0x0m0x <mx0mx@tuta.io>
2024-04-25 20:36:34 +0530

    add

---
+++ W/209.js
@@ -0,0 +1,8 @@
+/* 
+209: Constructor Functions and the new Operator
+*/
+// Import zone
+import { mainHead } from "../UTILS/text.js";
+// End import zone
+mainHead("209: Constructor Functions and the new Operator");
+console.log("hello");
