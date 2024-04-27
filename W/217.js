17eefc6a58780e85a4d11608160dfe1326faf703
m0x0m0x <mx0mx@tuta.io>
2024-04-27 22:39:40 +0530

    217

--- W/217.js
+++ W/217.js
@@ -7,3 +7,19 @@
 import { mainHead, paraText, subHead } from "../UTILS/text.js";
 // End import zone
 mainHead("217: Object.Create");
+
+// Implemening prototypal inheritance
+// Works differnt that classes
+
+const PersonProto = {
+  calcAge() {
+    console.log(2037 - this.birthYear);
+  },
+};
+
+const ste = Object.create(PersonProto);
+console.log(ste);
+
+ste.name = "Ste";
+ste.birthYear = 2000;
+ste.calcAge();
