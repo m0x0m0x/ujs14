8624f155d0acc9d878d6c961584205eaeb68129f
m0x0m0x <mx0mx@tuta.io>
2024-04-28 05:03:17 +0530

    add

--- W/219.js
+++ W/219.js
@@ -7,3 +7,5 @@
 import { mainHead, paraText, subHead } from "../UTILS/text.js";
 // End import zone
 mainHead("219: Inheritance Between Classes: Constructor Functions");
+
+subHead("Inheriting Classes between constructor functions");
