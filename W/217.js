695e88a8e4f140076fe51b20e06f4ad4380469d5
m0x0m0x <mx0mx@tuta.io>
2024-04-27 22:44:10 +0530

    216

--- W/217.js
+++ W/217.js
@@ -15,6 +15,13 @@ const PersonProto = {
   calcAge() {
     console.log(2037 - this.birthYear);
   },
+
+  // new method
+  init(firstName, birthYear, fetish) {
+    this.firstName = firstName;
+    this.birthYear = birthYear;
+    this.fetish = fetish;
+  },
 };
 
 const ste = Object.create(PersonProto);
@@ -23,3 +30,11 @@ console.log(ste);
 ste.name = "Ste";
 ste.birthYear = 2000;
 ste.calcAge();
+
+paraText("Checking that a new proto has been created");
+console.log(ste.__proto__);
+
+paraText("Making new person");
+const ina = Object.create(PersonProto);
+ina.init("ina", 1969, "boobs");
+ina.calcAge();
