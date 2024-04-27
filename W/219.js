2332d1cfea9ede314ff0b6738743569d317436d0
m0x0m0x <mx0mx@tuta.io>
2024-04-27 23:40:05 +0530

    219

---
+++ W/219.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* 
+219: Inheritance Between Classes: Constructor Functions
+*/
+// Import zone
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+// End import zone
+mainHead("219: Inheritance Between Classes: Constructor Functions");
