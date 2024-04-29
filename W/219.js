7e42433d80242fec9dee71897277b1cd64189bf3
m0x0m0x <mx0mx@tuta.io>
2024-04-29 06:33:40 +0530

    219

--- W/219.js
+++ W/219.js
@@ -9,3 +9,18 @@ import { mainHead, paraText, subHead } from "../UTILS/text.js";
 mainHead("219: Inheritance Between Classes: Constructor Functions");
 
 subHead("Inheriting Classes between constructor functions");
+// Setting up the person function constructor
+
+const Per1 = function (firstName, birthYear, fetish) {
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+  this.fetish = fetish;
+};
+
+// Sett a calc age method on the prototype
+Per1.prototype.calcAge = function () {
+  console.log(new Date().getFullYear() - this.birthYear);
+};
+
+const jes = new Per1("jess", 2009, "spit");
+jes.calcAge();
