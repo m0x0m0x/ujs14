f758c09a0352c87d4191b1bec25d1fe4da8b4869
m0x0m0x <mx0mx@tuta.io>
2024-04-29 23:29:37 +0530

    225

---
+++ W/225.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* 
+225: Encapsulation: Private Class Fields and Methods 
+*/
+// Import zone
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+// End import zone
+mainHead("225: Encapsulation: Private Class Fields and Methods ");
