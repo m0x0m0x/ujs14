b9f8c76e440b7e8a02efc3f172706d0e00d5f84c
m0x0m0x <mx0mx@tuta.io>
2024-04-30 11:58:40 +0530

    226

---
+++ W/226.js
@@ -0,0 +1,9 @@
+"use strict";
+
+/* 
+226: Chaining Methods
+*/
+// Import zone
+import { mainHead, paraText, subHead } from "../UTILS/text.js";
+// End import zone
+mainHead("226: Chaining Methods ");
